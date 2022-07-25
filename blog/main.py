ab0641ff52c76fa44d001d85caa95d8ee37ad079
Seyed Hosein Mousavi Fard <shmf1385@protonmail.com>
2022-07-25 15:38:03 +0430

    Add models

--- blog/main.py
+++ blog/main.py
@@ -8,4 +8,5 @@ app = FastAPI(name="Devslife Blog", version="1.0")
 
 @app.on_event('startup')
 def on_startup():
+    database.Base.metadata.create_all(bind=database.engine)
     db: Session = database.SessionLocal
---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(25))
+    email = Column(String(30))
+    password = Column(String(130))
+    profile_image = Column(Integer, ForeignKey("images.id"))
+
+    blogs = relationship("Blog", back_populates="writer")
+
+
+class Blog(Base):
+    __tablename__ = "blogs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50))
+    description = Column(String(120), nullable=True)
+    date = Column(Date)
+    content = Column(String(2400))
+
+    writer = relationship("User", back_populates="blogs")
+
+
+class Image(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String(50))
